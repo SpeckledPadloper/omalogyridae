9ea9c7eb20c7a947e542678bb8677be3c134af98
Laura frouke Wiedijk <lwiedijk@student.codam.nl>
2022-10-11 17:13:23 +0200

    pwd basics working

--- src/builtins/padloper_pwd.c
+++ src/builtins/padloper_pwd.c
@@ -6,24 +6,29 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/13 10:01:06 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2022/10/06 11:35:36 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2022/10/11 17:10:53 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 #include "../executer/hdr/executer.h"
 #include "../hdr/structs.h"
+#include <limits.h>
 
 void	padloper_pwd(t_metadata *data, t_exec_list_sim *cmd_list)
 {
+	char buf[PATH_MAX];
+
 	if (!data || !cmd_list)
 	{
 		fprintf(stderr, "Parameters are NULL");
 		exit(1);
 	}
-	fprintf(stderr, "check pwd\n");
-	write(STDOUT_FILENO, "pwd go to pipe\n", 15);
-	exit(0);
+	if (!getcwd(buf, sizeof(buf)))
+		print_error_exit("getcwd", errno, EXIT_FAILURE);
+	printf("%s\n", buf);
+	return ;
 }
