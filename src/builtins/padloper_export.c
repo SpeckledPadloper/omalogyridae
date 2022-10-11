b0e9f44dbb2d58b3e5de3d003aa71a9d312a5667
Laura frouke Wiedijk <lwiedijk@student.codam.nl>
2022-10-11 15:12:58 +0200

    export cleaner

--- src/builtins/padloper_export.c
+++ src/builtins/padloper_export.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/13 10:01:06 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2022/10/11 14:46:03 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2022/10/11 15:11:50 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ sort array
 char	*export_strcpy(char *dest, char *src)
 {
 	int	i;
-	int j;
+	int	j;
 
 	i = 0;
 	j = 0;
@@ -56,32 +56,34 @@ char	*export_strcpy(char *dest, char *src)
 			j += 2;
 		}
 		if (src[i] == '\0')
-			break ; 
+			break ;
 		dest[j] = src[i];
 		i++;
 		j++;
 	}
 	dest[j] = '"';
 	dest[j + 1] = '\0';
+	if (i == j)
+		dest[j] = '\0';
 	return (dest);
 }
 
-void populate_export(int size, char **src, char **dst)
+void	populate_export(int size, char **src, char **dst)
 {
-	int i;
-	int j;
-	
+	int	i;
+	int	j;
+
 	i = 0;
-	while(i < size)
+	while (i < size)
 	{
 		j = 0;
-		while(src[i][j])
+		while (src[i][j])
 			j++;
-		dst[i] = (char*)malloc(sizeof(char) * j + 3);
+		dst[i] = (char *)malloc(sizeof(char) * j + 3);
 		if (!dst)
 			print_error_exit("malloc", errno, EXIT_FAILURE);
 		export_strcpy(dst[i], src[i]);
-		i++;	
+		i++;
 	}
 	dst[i] = NULL;
 }
@@ -103,7 +105,7 @@ int	envcmp(char *s1, char *s2)
 
 bool	var_not_valid(char *var)
 {
-	int i;
+	int	i;
 
 	i = 1;
 	if (!(ft_isalpha(var[0]) || var[0] == '_'))
@@ -119,14 +121,14 @@ bool	var_not_valid(char *var)
 
 void	add_var(t_metadata *data, t_exec_list_sim *cmd_list)
 {
-	char **temp_env;
-	bool found;
-	int i;
-	int j;
+	char	**temp_env;
+	bool	found;
+	int		i;
+	int		j;
 
 	i = 1;
 	j = 0;
-	while(cmd_list->cmd[i])
+	while (cmd_list->cmd[i])
 	{
 		printf("hallo cmd is : %s\n", cmd_list->cmd[i]);
 		if (var_not_valid(cmd_list->cmd[i]))
@@ -138,7 +140,7 @@ void	add_var(t_metadata *data, t_exec_list_sim *cmd_list)
 		data->env_updated = true;
 		j = 0;
 		found = false;
-		while(data->padloper_envp[j])
+		while (data->padloper_envp[j])
 		{
 			if (!envcmp(data->padloper_envp[j], cmd_list->cmd[i])
 				|| envcmp(data->padloper_envp[j], cmd_list->cmd[i]) == EXISTING_VAR_HAS_NO_VALUE)
@@ -157,7 +159,7 @@ void	add_var(t_metadata *data, t_exec_list_sim *cmd_list)
 		if (found)
 		{
 			i++;
-			continue;
+			continue ;
 		}
 		printf("!!add new variable: %s\n", cmd_list->cmd[i]);
 		temp_env = allocate_env(data->padloper_envp, &(data->envp_size), false, true);
@@ -167,22 +169,22 @@ void	add_var(t_metadata *data, t_exec_list_sim *cmd_list)
 		add_env(data->padloper_envp, cmd_list->cmd[i], data->envp_size - 1);
 		i++;
 	}
-	
 }
 
 void	sort_env(t_metadata *data)
 {
-	int i;
-	int j;
-	char *temp;
+	int		i;
+	int		j;
+	char	*temp;
 
 	i = 0;
-	while(i < data->envp_size)
+	while (i < data->envp_size)
 	{
 		j = 1 + i;
-		while(j < data->envp_size)
+		while (j < data->envp_size)
 		{
-			if (envcmp(data->sorted_print_export[i], data->sorted_print_export[j]) > 0)
+			if (envcmp(data->sorted_print_export[i],
+					data->sorted_print_export[j]) > 0)
 			{
 				temp = data->sorted_print_export[j];
 				data->sorted_print_export[j] = data->sorted_print_export[i];
@@ -196,23 +198,21 @@ void	sort_env(t_metadata *data)
 
 void	padloper_export(t_metadata *data, t_exec_list_sim *cmd_list)
 {
-	int i;
+	int	i;
 
-	printf("entering export = [%d]\n", data->env_updated);
 	if (cmd_list->cmd[1])
 	{
-		printf("before add var = [%d]\n", data->env_updated);
 		add_var(data, cmd_list);
-		printf("after add var = [%d]\n", data->env_updated);
 		return ;
 	}
 	if (data->env_updated)
 	{
-		printf("!!hoi update array\n");
 		if (data->sorted_print_export)
 			free_2d_array(data->sorted_print_export);
-		data->sorted_print_export = allocate_env(data->padloper_envp, &(data->envp_size), false, false);
-		populate_export(data->envp_size, data->padloper_envp, data->sorted_print_export);
+		data->sorted_print_export = allocate_env
+			(data->padloper_envp, &(data->envp_size), false, false);
+		populate_export(data->envp_size, data->padloper_envp,
+			data->sorted_print_export);
 		sort_env(data);
 		data->env_updated = false;
 	}
