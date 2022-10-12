5e08dcd280e820c6549a44da876e05096ff40269
Laura frouke Wiedijk <lwiedijk@student.codam.nl>
2022-10-12 16:57:20 +0200

    cd starters

--- src/builtins/padloper_cd.c
+++ src/builtins/padloper_cd.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/13 10:01:06 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2022/10/06 11:33:34 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2022/10/12 16:46:11 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 #include <stdlib.h>
 #include "../executer/hdr/executer.h"
 #include "../hdr/structs.h"
+#include "../../libft/libft.h"
 
 void	padloper_cd(t_metadata *data, t_exec_list_sim *cmd_list)
 {
-	if (!data || !cmd_list)
-	{
-		fprintf(stderr, "Parameters are NULL");
-		exit(1);
-	}
+	if (!cmd_list->cmd[1] || cmd_list->cmd[2])
+		printf("too many or to little args\n");
+	else
+		chdir(cmd_list->cmd[1]);
 	fprintf(stderr, "check cd\n");
 	write(STDOUT_FILENO, "cd go to pipe\n", 14);
-	exit(0);
+	return ;
 }
