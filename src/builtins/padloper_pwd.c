54e8ee8946112d991c0974cf3d8ca0a5ea2fc90f
Laura frouke Wiedijk <lwiedijk@student.codam.nl>
2022-10-13 09:22:36 +0200

    improve error handling

--- src/builtins/padloper_pwd.c
+++ src/builtins/padloper_pwd.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/13 10:01:06 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2022/10/12 16:27:45 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2022/10/13 09:18:59 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,12 @@ void	padloper_pwd(t_metadata *data, t_exec_list_sim *cmd_list)
 {
 	char buf[PATH_MAX];
 
-	if (cmd_list->cmd[1] && cmd_list->cmd[1][0] == '-')
+	if (cmd_list->cmd[1] && cmd_list->cmd[1][1] && cmd_list->cmd[1][0] == '-')
+	{
+		if (cmd_list->cmd[1][2])
+			cmd_list->cmd[1][2] = '\0';
 		builtin_error("pwd: ", cmd_list->cmd[1], NOT_SUPPORTED, data);
+	}
 	else 
 	{
 		if (!getcwd(buf, sizeof(buf)))
