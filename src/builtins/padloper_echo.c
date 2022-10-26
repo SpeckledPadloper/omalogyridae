e2705718fba524c6bd428ff94d9d7693f31905a5
Laura frouke Wiedijk <lwiedijk@student.codam.nl>
2022-10-26 16:05:30 +0200

    echo -nnnnnn

--- src/builtins/padloper_echo.c
+++ src/builtins/padloper_echo.c
@@ -6,7 +6,7 @@
 /*   By: lwiedijk <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/13 10:01:06 by lwiedijk      #+#    #+#                 */
-/*   Updated: 2022/10/18 16:18:45 by lwiedijk      ########   odam.nl         */
+/*   Updated: 2022/10/26 16:02:38 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,37 @@
 #include "../hdr/structs.h"
 #include "../../libft/libft.h"
 
+
+bool	check_option(t_exec_list_sim *cmd_list, int *i)
+{
+	int		j;
+	bool	no_newline_flag;
+
+	no_newline_flag = false;
+	while (cmd_list->cmd[*i] && !ft_strncmp(cmd_list->cmd[*i], "-n", 2))
+	{
+		j = 1;
+		while(cmd_list->cmd[*i][j] && cmd_list->cmd[*i][j] == 'n')
+			j++;
+		if (cmd_list->cmd[*i][j] && cmd_list->cmd[*i][j] != 'n')
+			break;
+		no_newline_flag = true;
+		(*i)++;
+	}
+	return (no_newline_flag);
+}
+
 void	padloper_echo(t_metadata *data, t_exec_list_sim *cmd_list)
 {
 	int		i;
+	int		j;
 	bool	no_newline_flag;
 
 	i = 1;
 	no_newline_flag = false;
 	if (!cmd_list->cmd[1])
 		printf("\n");
-	while (cmd_list->cmd[i] && !ft_strcmp(cmd_list->cmd[i], "-n"))
-	{
-		no_newline_flag = true;
-		i++;
-	}
+	no_newline_flag = check_option(cmd_list, &i);
 	while (cmd_list->cmd[i])
 	{
 		if (!cmd_list->cmd[i + 1])
