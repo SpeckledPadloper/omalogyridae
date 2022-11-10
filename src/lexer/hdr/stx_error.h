1f1c0babc99972b4e8c0b10790817c5ee62efb3c
Laura frouke Wiedijk <lwiedijk@student.codam.nl>
2022-11-10 10:39:51 +0100

    include protect of stx_error norm, fix bug in cd when OLDPWD is created

--- src/lexer/hdr/stx_error.h
+++ src/lexer/hdr/stx_error.h
@@ -6,12 +6,12 @@
 /*   By: mteerlin <mteerlin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/18 19:39:40 by mteerlin      #+#    #+#                 */
-/*   Updated: 2022/11/09 20:39:02 by mteerlin      ########   odam.nl         */
+/*   Updated: 2022/11/10 08:50:58 by lwiedijk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STX_ERRORS_H
-# define STX_ERRORS_H
+#ifndef STX_ERROR_H
+# define STX_ERROR_H
 # include "../../hdr/structs.h"
 # include <stdbool.h>
 
